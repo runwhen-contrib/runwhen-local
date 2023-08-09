f52d3cefc889b5c44dbbb902f6a5d32dd6b297a3
Rob Vaterlaus <rob.vaterlaus@gmail.com>
2023-08-09 04:43:15 -0700

    Fix for an error reporting bug, plus removal of some cruft (#238)
    - the REST service and the run.py script weren't consistent about the name
      of the field in the response for the error message
    - a bunch of the old workspace upload test code somehow got resurrected in
      the new repo
    - ditto for the old prodgraph directory, which had been renamed to
      workspace_builder
    - fixed a build error with jinja2 by adding it to pyproject.toml
    
    Co-authored-by: Rob Vaterlaus <rob.vaterlaus@runwhen.com>

--- src/prodgraph/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-from component import init_components
-
-class ProdgraphConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'prodgraph'
-
-    def ready(self):
-        init_components()
