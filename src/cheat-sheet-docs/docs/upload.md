12372267ef6b7fadc219f6a19fcf10541fba905c
Shea Stewart <stewart.shea@gmail.com>
2023-09-25 21:47:39 +0000

    terminal format fixes

---
+++ src/cheat-sheet-docs/docs/upload.md
@@ -0,0 +1,9 @@
+# RunWhen Platform Upload
+
+## Login to RunWhen Platform Beta Instance
+https://app.beta.runwhen.com/
+
+## Create a workspace
+https://app.beta.runwhen.com/?addWorkspace=true
+
+## Download workspace tethering configuration file 
--- src/server.js
+++ src/server.js
@@ -34,16 +34,33 @@ wss.on('connection', (ws) => {
         clearInterval(interval);
     });
 
-    // Spawn a new shell process for each WebSocket connection
-    const shell = spawn('bash');
+    const shell = spawn('bash', [], {
+        cols: 80,
+        rows: 30,
+        cwd: process.env.HOME,
+        env: process.env
+    });
 
     // When data is received from the shell, send it to the WebSocket
     shell.on('data', (data) => {
         ws.send(data);
     });
 
-    // When data is received from the WebSocket, send it to the shell
-    ws.on('message', (msg) => {
-        shell.write(msg);
+    ws.on('message', (data) => {
+        const msg = data.toString(); // Convert the binary data to a string
+        console.log(`Backend terminal dimensions: ${shell.cols}x${shell.rows}`);
+        if (msg.startsWith("resize:")) {
+            const parts = msg.split(":")[1].split(",");
+            const cols = parseInt(parts[0], 10);
+            const rows = parseInt(parts[1], 10);
+    
+            if (!isNaN(cols) && !isNaN(rows)) {
+                shell.resize(cols, rows);
+            } else {
+                console.error("Invalid resize parameters:", msg);
+            }
+        } else {
+            shell.write(msg);
+        }
     });
 });
