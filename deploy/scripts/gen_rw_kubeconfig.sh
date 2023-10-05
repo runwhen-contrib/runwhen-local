be30bf6fd03e9bfc0ad2f75d52312d7e6b09c44f
Shea Stewart <stewart.shea@gmail.com>
2023-10-05 11:05:23 +0000

    update kubeconfig for multi-user support

--- deploy/scripts/gen_rw_kubeconfig.sh
+++ deploy/scripts/gen_rw_kubeconfig.sh
@@ -12,80 +12,9 @@
 # Author: @stewartshea
 #######################################
 
-
-#######################################
-# Extract OCI command function by combining command and args
-# Note: This is possibly reusable as we add various providers
-#
-# Arguments: 
-#   path to shared kubeconfig which has parameters to build up the command
-# Returns: 
-#   full command to be executed
-#######################################
-extract_oci_command() {
-    kubeconfig_file=$1
-    
-    # Extract command
-    cmd=$(yq e '.users[] | select(.user.exec.command == "oci").user.exec.command' "$kubeconfig_file")
-
-    # Extract args
-    args=$(yq e '.users[] | select(.user.exec.command == "oci").user.exec.args[]' "$kubeconfig_file" | tr '\n' ' ')
-
-    # Construct full command
-    echo "$cmd $args"
-}
-
-#######################################
-# Extract OIDC command function by combining command and args
-#
-# Arguments: 
-#   path to shared kubeconfig which has parameters to build up the command
-# Returns: 
-#   full command to be executed
-#######################################
-extract_oidc_command() {
-    kubeconfig_file=$1
-
-    # Check if the command is 'kubectl' with 'oidc-login' in the args
-    oidc_exists=$(yq e '.users[] | select(.user.exec.command == "kubectl" and .user.exec.args[] == "oidc-login")' "$kubeconfig_file")
-
-    # If it doesn't exist, return nothing
-    if [ -z "$oidc_exists" ]; then
-        echo ""
-        return
-    fi
-
-    # Extract args
-    args=$(yq e '.users[] | select(.user.exec.command == "kubectl" and .user.exec.args[] == "oidc-login").user.exec.args[]' "$kubeconfig_file" | tr '\n' ' ')
-
-    # Construct full command
-    echo "kubectl $args"
-}
-
-#######################################
-# Extract kubeLogin command function by combining command and args
-#
-# Arguments: 
-#   path to shared kubeconfig which has parameters to build up the command
-# Returns: 
-#   full command to be executed
-#######################################
-extract_kubelogin_command() {
-    kubeconfig_file=$1
-    
-    # Extract command
-    cmd=$(yq e '.users[] | select(.user.exec.command == "kubelogin").user.exec.command' "$kubeconfig_file")
-
-    # Extract args
-    args=$(yq e '.users[] | select(.user.exec.command == "kubelogin").user.exec.args[]' "$kubeconfig_file" | tr '\n' ' ')
-
-    # Construct full command
-    echo "$cmd $args"
-}
-
-
 # Set the working directory
 workdir="$workdir"
+
 
 # Check if the workdir/shared folder exists
 if [ -d "$workdir/shared" ]; then
@@ -95,8 +24,16 @@ else
   exit 1
 fi
 
-# Copy ~/.kube/config to $shared_dir/kubeconfig
-cp ~/.kube/config "$workdir/shared/kubeconfig"
+# Use KUBECONFIG if set, else default to ~/.kube/config
+if [ -z "${KUBECONFIG}" ]; then
+    kubeconfig_path="$HOME/.kube/config"
+else
+    kubeconfig_path="${KUBECONFIG}"
+fi
+
+# Copy kubeconfig to $shared_dir/kubeconfig
+cp "$kubeconfig_path" "$workdir/shared/kubeconfig"
+
 
 # Convert YAML to JSON
 kubeconfig_json=$(yq eval -o=json "$workdir/shared/kubeconfig")
@@ -107,41 +44,40 @@ oci_exists=$(yq e '.users[] | select(.user.exec.command == "oci")' "$workdir/sha
 oidc_exists=$(yq e '.users[] | select(.user.exec.command == "kubectl").user.exec.args[]' "$workdir/shared/kubeconfig" | grep "oidc-login")
 kubelogin_exists=$(yq e '.users[] | select(.user.exec.command == "kubelogin")' "$workdir/shared/kubeconfig")
 
-# Generate tokens only if commands exist in kubeconfig
-gke_token=""
-[ ! -z "$gke_exists" ] && gke_token=$(gke-gcloud-auth-plugin generate-token "$user" | jq -r .status.token)
+### New bits
+users_count=$(yq e '.users|length' "$workdir/shared/kubeconfig")
 
-oci_token=""
-[ ! -z "$oci_exists" ] && oci_token=$($(extract_oci_command $workdir/shared/kubeconfig) | jq -r .status.token)
+# Iterate over each user stanza in the kubeconfig to generate unique tokens
+for ((i = 0; i < $users_count; i++)); do
 
-oidc_token=""
-[ ! -z "$oidc_exists" ] && oidc_token=$($(extract_oidc_command $workdir/shared/kubeconfig) | jq -r .status.token)
+    # Extract the command and args for the current user
+    user_command=$(yq e ".users[$i].user.exec.command" "$workdir/shared/kubeconfig")
+    user_args=$(yq e ".users[$i].user.exec.args[]" "$workdir/shared/kubeconfig" | tr '\n' ' ')
+    # Generate token based on the command and args
+    case $user_command in
+        gke-gcloud-auth-plugin)
+            token=$(gke-gcloud-auth-plugin $user_args | jq -r .status.token)
+            ;;
+        oci)
+            token=$(oci $user_args | jq -r .status.token)
+            ;;
+        kubectl)
+            token=$(kubectl $user_args | jq -r .status.token)
+            ;;
+        kubelogin)
+            token=$(kubelogin $user_args | jq -r .status.token)
+            ;;
+        *)
+            token=""
+            ;;
+    esac
 
-kubelogin_token=""
-[ ! -z "$kubelogin_exists" ] && kubelogin_token=$($(extract_kubelogin_command $workdir/shared/kubeconfig) | jq -r .status.token)
-
-# Process the JSON
-processed_json=$(echo "$kubeconfig_json" | jq --arg gke_token "$gke_token" --arg oci_token "$oci_token" --arg oidc_token "$oidc_token" --arg kubelogin_token "$kubelogin_token" '
-  .users[] |= 
-    if .user.exec.command == "gke-gcloud-auth-plugin" then 
-      .user.token = $gke_token 
-      | del(.user.exec) 
-    elif .user.exec.command == "kubelogin" then 
-      .user.token = $kubelogin_token 
-      | del(.user.exec) 
-    elif .user.exec.command == "oci" then 
-      .user.token = $oci_token 
-      | del(.user.exec) 
-    elif .user.exec.command == "kubectl" and (.user.exec.args | contains(["oidc-login"])) then 
-      .user.token = $oidc_token 
-      | del(.user.exec)
-    else 
-      . 
-    end
-')
+    # Update the user stanza with the generated token
+    kubeconfig_json=$(echo "$kubeconfig_json" | jq --argjson i $i --arg token $token '.users[$i].user.token = $token | del(.users[$i].user.exec)')
+done
 
 # Convert JSON back to YAML and overwrite shared kubeconfig
-echo "$processed_json" | yq eval -P - > "$workdir/shared/kubeconfig"
+echo "$kubeconfig_json" | yq eval -P - > "$workdir/shared/kubeconfig"
 
 
 # Set permissions for container to read the file
