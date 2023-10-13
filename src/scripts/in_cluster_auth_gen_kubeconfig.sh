a83ee52ff206505d1c61cf529d5b24e062aae050
Shea Stewart <stewart.shea@gmail.com>
2023-10-13 23:59:16 +0000

    in-cluster-script-updates

--- src/scripts/in_cluster_auth_gen_kubeconfig.sh
+++ src/scripts/in_cluster_auth_gen_kubeconfig.sh
@@ -2,36 +2,39 @@
 
 # Variables
 SERVER_DETAILS="$1"
-CA_CERT_PATH="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-
-# Check if the kubeconfig file is provided
-if [[ -z "$1" ]]; then
-    echo "Please provide the kubeconfig file as an argument."
-    exit 1
-fi
-
 KUBECONFIG_FILE="/shared/in_cluster_kubeconfig.yaml"
 NEW_KUBECONFIG_FILE="/shared/generated-kubeconfig.yaml"
 
-# Check if the CA cert file exists
-if [[ ! -f "$CA_CERT_PATH" ]]; then
-    echo "CA certificate not found at $CA_CERT_PATH."
+# Check if the server details are provided
+if [[ -z "$SERVER_DETAILS" ]]; then
+    echo "Please provide the server details as an argument."
     exit 1
 fi
 
-# Embed the CA certificate
-CA_CERT_CONTENT=$(sed ':a;N;$!ba;s/\n/\\n/g' "$CA_CERT_PATH")
-
-# Substitute server details and embed the CA certificate
-sed -e "s|server:.*|server: $SERVER_DETAILS|" \
-    -e "s|certificate-authority:.*|certificate-authority-data: $CA_CERT_CONTENT|" \
-    "$KUBECONFIG_FILE" > "$NEW_KUBECONFIG_FILE"
-
+# Check if certificate-authority is a file path
+if grep -q "certificate-authority:" "$KUBECONFIG_FILE"; then
+    CA_CERT_PATH=$(grep "certificate-authority:" "$KUBECONFIG_FILE" | awk '{print $2}')
+    if [[ -f "$CA_CERT_PATH" ]]; then
+        # Embed the CA certificate
+        CA_CERT_CONTENT=$(base64 -w 0 "$CA_CERT_PATH")
+        # Substitute server details and embed the CA certificate
+        sed -e "s|^  server:.*|  server: $SERVER_DETAILS|" \
+            -e "/certificate-authority:/a \    certificate-authority-data: $CA_CERT_CONTENT" \
+            -e "/certificate-authority:/d" \
+            "$KUBECONFIG_FILE" > "$NEW_KUBECONFIG_FILE"
+    else
+        echo "CA certificate path specified in kubeconfig is not valid."
+        exit 1
+    fi
+else
+    # If certificate-authority-data is already present, just update server details
+    sed "s|^  server:.*|  server: $SERVER_DETAILS|" "$KUBECONFIG_FILE" > "$NEW_KUBECONFIG_FILE"
+fi
 
 echo "Modified kubeconfig saved to $NEW_KUBECONFIG_FILE"
 
 # Validate the kubeconfig
-if kubectl --kubeconfig="$NEW_KUBECONFIG_FILE" get api-versions > /dev/null 2>&1; then
+if kubectl --kubeconfig="$NEW_KUBECONFIG_FILE" get ns > /dev/null 2>&1; then
     echo "Kubeconfig is valid!"
 else
     echo "Kubeconfig validation failed."
