92b0cb12d43e24dcae5ce6795bd3c057e484d572
Shea Stewart <stewart.shea@gmail.com>
2024-01-03 15:37:07 +0000

    fixes #403

--- src/cheatsheet.py
+++ src/cheatsheet.py
@@ -161,7 +161,7 @@ def cmd_expansion(keyword_arguments, parsed_runbook_config):
         raw_script_url=generate_raw_git_url(git_url=parsed_runbook_config["spec"]["codeBundle"]["repoUrl"], ref=parsed_runbook_config["spec"]["codeBundle"]["ref"], file_path=file_path)
         env=""
         for var in parsed_runbook_config["spec"]["configProvided"]:
-            env += f"{var['name']}={var['value']};"
+            env += f"{var['name']}={var['value']} "
         matched_cmd_override = None
         for arg in cmd_components:
             arg=arg.strip()
@@ -174,9 +174,9 @@ def cmd_expansion(keyword_arguments, parsed_runbook_config):
             cmd_parts=matched_cmd_override.split()
             cmd_arguments=' '.join(cmd_parts[1:])
             cmd_arguments=cmd_arguments.strip("'")
-            cmd_components[0]=f"{env} curl -s {raw_script_url} | bash -s {cmd_arguments}"
+            cmd_components[0]=f"{env} bash -c \"$(curl -s {raw_script_url})\" _ {cmd_arguments}"
         else: 
-            cmd_components[0]=f"{env} curl -s {raw_script_url} | bash"
+            cmd_components[0]=f"{env} bash -c \"$(curl -s {raw_script_url})\" _"
         logger.debug(f"Rendering bash file after split: {cmd_components}") 
         cmd_str=cmd_components[0]      
     else: 
