4e03704f9081dddb846cb77b71150fbd89ba9e78
Rob Vaterlaus <rob.vaterlaus@gmail.com>
2024-03-26 06:35:04 -0700

    Don't terminate on generation rule parse errors + misc cleanup (#449)
    - added checks for both an completely unparseable generation rules file
      (i.e. can't be parsed as YAML) and individual gen rule data that
      doesn't conform to the expected format. The first case should be rare
      and would indicate a bug in a code collection. The second is more
      expected and would happen if the user is running an older version of RWL
      against a code collection that is using newer generation rule features
      (e.g. a new match rule type). In that case we don't want to complete
      fail out of the run of the workspace builder but instead just ignore
      the offending gen rule and continue with the gen rules that could be
      parsed. In both error cases an warning message is returned to the user
      to indicate that some gen rules were skipped and that they need to
      update to a newer/current version of RWL to enable the skipped rules.
    - don't treat the workspace info as a resource that's stored in the
      resource database. This was an artifact of the old neo4j code that wasn't
      cleaned up when neo4j was removed. Instead the workspace info is just
      constructed as a dict but still made available to the templates in
      the same way as before. This included getting rid of the
      runwhen_default_workspace component that handled creating the workspace
      resource.
    - cleaned up the test runner code to load the workspace info from a YAML
      file instead of having a preparsed dict version of the data inline in the
      code, which was painful to maintain and led to unnecessary discrepancies
      in how the test code ran vs the run.py script
    
    Co-authored-by: Rob Vaterlaus <rob.vaterlaus@runwhen.com>
    Co-authored-by: Shea Stewart <stewart.shea@gmail.com>

--- src/workspace_builder/tests.py
+++ src/workspace_builder/tests.py
@@ -7,74 +7,26 @@ import time
 from base64 import b64decode
 from base64 import b64encode
 from http import HTTPStatus
-from typing import Any, Union
+from typing import Any
+from utils import read_file
+import yaml
 
 from django.test import TestCase
 
 from exceptions import WorkspaceBuilderException
 from utils import transform_client_cloud_config
 
-TEST_OUTPUT_DIRECTORY = "TestOutput"
-
 git_repo_root = os.getenv("WB_GIT_REPO_ROOT")
 if not git_repo_root:
     print("The WB_GIT_REPO_ROOT environment variable must be set to directory where the "
           "code collection repos are cloned.")
 
-
-# FIXME: Should get rid of this embedded JSON version of the workspace info file and just
-# load it from an actual workspace info file.
-BASE_REQUEST_DATA = {
-    "defaultLOD": 2,
-    "defaultWorkspaceName": "my-workspace",
-    "workspaceName": "my-workspace",
-    "workspaceOwnerEmail": "test@runwhen.com",
-    "workspaceOutputPath": "workspace",
-    "defaultLocation": "northamerica-northeast2-01",
-    "resourceDumpPath": "resource-dump.yaml",
-    # "resourceLoadFile": "resource-dump.yaml",
-    "mapCustomizationRules": "map-customization-rules-test",
-    "codeCollections": [
-        {"repoURL": f"file://{git_repo_root}/rw-public-codecollection", "branch": "main"},
-        {"repoURL": f"file://{git_repo_root}/rw-cli-codecollection", "branch": "main"}
-    ],
-    "cloudConfig": {
-        # "azure": {
-        #     # To enable testing of the cloudquery Azure indexing you need to
-        #     # define the 4 environments variables below either in the shell from
-        #     # which you're running the test or the run configuration in whatever
-        #     # IDE you're using. These environment variables are described in the
-        #     # documentation for the CloudQuery Azure plugin:
-        #     # https://hub.cloudquery.io/plugins/source/cloudquery/azure/v9.3.7/docs
-        #     "subscriptionId": os.getenv("WB_AZURE_SUBSCRIPTION_ID"),
-        #     "tenantId": os.getenv("WB_AZURE_TENANT_ID"),
-        #     "clientId": os.getenv("WB_AZURE_CLIENT_ID"),
-        #     "clientSecret": os.getenv("WB_AZURE_CLIENT_SECRET"),
-        #     "resourceGroupLevelOfDetails": {
-        #         "WorkspaceBuilderTesting": "basic"
-        #     }
-        # },
-        "kubernetes": {
-            "kubeconfigFile": "kubeconfig",
-            "namespaceLODs": {"kube-system": 0, "kube-public": 0},
-        # }
-        # "gcp": {
-        #     "applicationCredentialsFile": "GCPServiceAccountKeyWorkspaceBuilder.json",
-        #     "projects": ["iron-radio-408515"],
-        #     "projectLevelOfDetails": {
-        #        "iron-radio-408515": "basic"
-        #     }
-        # }
-    }
-}
-
-def read_file(file_path: str, mode="r") -> Union[str, bytes]:
-    with open(file_path, mode) as f:
-        return f.read()
+workspace_info_name_default = os.getenv("WB_WORKSPACE_INFO_NAME", "workspaceInfo.yaml")
+base_directory_default = os.getenv("WB_BASE_DIRECTORY", "shared")
 
 
-def build_rest_request_data(base_data: dict[str, Any], **kwargs) -> str:
-    request_data = base_data.copy()
+def build_rest_request_data(base_directory, workspace_info_data: dict[str, Any], **kwargs) -> str:
+    request_data = workspace_info_data.copy()
     for key, value in kwargs.items():
         request_data[key] = value
     for key in request_data.keys():
@@ -89,11 +41,16 @@ def build_rest_request_data(base_data: dict[str, Any], **kwargs) -> str:
             request_data[key] = b64encode(data).decode('utf-8')
         elif key == "cloudConfig":
             cloud_config_data = request_data[key]
-            transform_client_cloud_config(cloud_config_data)
+            transform_client_cloud_config(base_directory, cloud_config_data)
         elif key == 'mapCustomizationRules':
             map_customization_rules_path = request_data[key]
             # FIXME: This code is (mostly) copied from run.py.
             # Should figure out a way to factor this to eliminate the code duplication
+            # FIXME: Seems like the path should be resolved relative to the base directory?
+            # Otherwise, I don't see how you could load non-default map customizations from
+            # the shared directory?
+            # The logic in run.py is sort of weird and I can't remember why it's written that way.
+            # Should take another look at this to refresh my memory and clean it up.
             if not os.path.exists(map_customization_rules_path):
                 raise WorkspaceBuilderException("Map customization rules path does not exist")
             if os.path.isdir(map_customization_rules_path):
@@ -141,35 +98,38 @@ class ProductionComponentTestCase(TestCase):
     requirements that should gate release readiness.
     """
 
-    def run_common(self, components: str, base_request_data: dict[str, Any] = None):
-        if base_request_data is None:
-            base_request_data = BASE_REQUEST_DATA
-        request_data = build_rest_request_data(base_request_data, components=components)
+    def run_common(self, components: str, base_directory=None, workspace_info_name=None):
+        if base_directory is None:
+            base_directory = base_directory_default
+        if workspace_info_name is None:
+            workspace_info_name = workspace_info_name_default
+        workspace_info_path = os.path.join(base_directory, workspace_info_name)
+        workspace_info_text = read_file(workspace_info_path, "r")
+        workspace_info_data = yaml.safe_load(workspace_info_text)
+        request_data = build_rest_request_data(base_directory, workspace_info_data, components=components)
         response = self.client.post("/run/", data=request_data, content_type="application/json")
         self.assertEqual(HTTPStatus.OK, response.status_code)
         response_data = json.loads(response.content)
         archive_bytes = b64decode(response_data["output"])
         archive_file_obj = io.BytesIO(archive_bytes)
         archive = tarfile.open(fileobj=archive_file_obj, mode="r")
-        if os.path.exists(TEST_OUTPUT_DIRECTORY):
-            shutil.rmtree(TEST_OUTPUT_DIRECTORY)
-        os.makedirs(TEST_OUTPUT_DIRECTORY)
-        archive.extractall(TEST_OUTPUT_DIRECTORY)
+        output_directory_path = os.path.join(base_directory, "output")
+        if os.path.exists(output_directory_path):
+            shutil.rmtree(output_directory_path)
+        os.makedirs(output_directory_path)
+        archive.extractall(output_directory_path)
         print(response_data.get("message"))
         warnings = response_data.get("warnings")
         for warning in warnings:
             print("WARNING: " + warning)
 
     def test_generation_rules_workspace_gen(self):
-        self.run_common("load_resources,kubeapi,cloudquery,runwhen_default_workspace,generation_rules,render_output_items,dump_resources")
+        self.run_common("load_resources,kubeapi,cloudquery,generation_rules,render_output_items,dump_resources")
 
     # Need to implement some sort of dump/load feature to the resource registry
     # for this to make sense now that we're not using neo4j anymore.
     # def test_generation_rules_workspace_gen_no_indexing(self):
     #     self.run_common("generation_rules,render_output_items")
-
-    def test_dump_resources(self):
-        self.run_common("kubeapi,runwhen_default_workspace,generation_rules,render_output_items,dump_resources")
 
     def test_info(self):
         response = self.client.get("/info/")
