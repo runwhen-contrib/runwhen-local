8109afd33083f715167d2ef7dd11c4e924e0f4d2
Shea Stewart <stewart.shea@gmail.com>
2024-11-26 01:19:24 +0000

    auth template updates to match on kubernetes resource but support azure auth

--- .test/azure/multi-subscription-aks/terraform/main.tf
+++ .test/azure/multi-subscription-aks/terraform/main.tf
@@ -67,76 +67,76 @@ resource "azurerm_kubernetes_cluster" "cluster_1_aks" {
   }
 }
 
-# Cluster 2 Resources
+# # Cluster 2 Resources
 
-# Resource Group
-resource "azurerm_resource_group" "cluster_2_rg" {
-  provider = azurerm.cluster_2
-  name     = "azure-aks-2"
-  location = "West US"
-  tags = {
-    "env"       = "test"
-    "lifecycle" = "deleteme"
-    "product"   = "runwhen"
-  }
-}
+# # Resource Group
+# resource "azurerm_resource_group" "cluster_2_rg" {
+#   provider = azurerm.cluster_2
+#   name     = "azure-aks-2"
+#   location = "West US"
+#   tags = {
+#     "env"       = "test"
+#     "lifecycle" = "deleteme"
+#     "product"   = "runwhen"
+#   }
+# }
 
-# Managed Identity
-resource "azurerm_user_assigned_identity" "cluster_2_identity" {
-  provider            = azurerm.cluster_2
-  name                = "aks-cl-2-identity"
-  location            = azurerm_resource_group.cluster_2_rg.location
-  resource_group_name = azurerm_resource_group.cluster_2_rg.name
-}
+# # Managed Identity
+# resource "azurerm_user_assigned_identity" "cluster_2_identity" {
+#   provider            = azurerm.cluster_2
+#   name                = "aks-cl-2-identity"
+#   location            = azurerm_resource_group.cluster_2_rg.location
+#   resource_group_name = azurerm_resource_group.cluster_2_rg.name
+# }
 
-# Role Assignment for Service Principal
-resource "azurerm_role_assignment" "cluster_2_sp_owner" {
-  provider             = azurerm.cluster_2
-  scope                = "/subscriptions/${var.subscription_id_2}"
-  role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
-  principal_id         = var.sp_principal_id
-  principal_type       = "ServicePrincipal"
-}
+# # Role Assignment for Service Principal
+# resource "azurerm_role_assignment" "cluster_2_sp_owner" {
+#   provider             = azurerm.cluster_2
+#   scope                = "/subscriptions/${var.subscription_id_2}"
+#   role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
+#   principal_id         = var.sp_principal_id
+#   principal_type       = "ServicePrincipal"
+# }
 
-# Role Assignment for Service Principal
-resource "azurerm_role_assignment" "cluster_2_sp_reader" {
-  provider             = azurerm.cluster_1
-  scope                = "/subscriptions/${var.subscription_id_2}"
-  role_definition_name = "Reader"
-  principal_id         = var.sp_principal_id
-  principal_type       = "ServicePrincipal"
-}
+# # Role Assignment for Service Principal
+# resource "azurerm_role_assignment" "cluster_2_sp_reader" {
+#   provider             = azurerm.cluster_1
+#   scope                = "/subscriptions/${var.subscription_id_2}"
+#   role_definition_name = "Reader"
+#   principal_id         = var.sp_principal_id
+#   principal_type       = "ServicePrincipal"
+# }
 
-# AKS Cluster
-resource "azurerm_kubernetes_cluster" "cluster_2_aks" {
-  provider            = azurerm.cluster_2
-  name                = "aks-cl-2"
-  location            = azurerm_resource_group.cluster_2_rg.location
-  resource_group_name = azurerm_resource_group.cluster_2_rg.name
-  dns_prefix          = "aks-cl-2"
+# # AKS Cluster
+# resource "azurerm_kubernetes_cluster" "cluster_2_aks" {
+#   provider            = azurerm.cluster_2
+#   name                = "aks-cl-2"
+#   location            = azurerm_resource_group.cluster_2_rg.location
+#   resource_group_name = azurerm_resource_group.cluster_2_rg.name
+#   dns_prefix          = "aks-cl-2"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DC2s_v2"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_DC2s_v2"
+#   }
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.cluster_2_identity.id]
-  }
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [azurerm_user_assigned_identity.cluster_2_identity.id]
+#   }
 
-  azure_active_directory_role_based_access_control {
-    azure_rbac_enabled = true
-    tenant_id          = var.tenant_id
-  }
+#   azure_active_directory_role_based_access_control {
+#     azure_rbac_enabled = true
+#     tenant_id          = var.tenant_id
+#   }
 
-  tags = {
-    "env"       = "test"
-    "lifecycle" = "deleteme"
-    "product"   = "runwhen"
-  }
-}
+#   tags = {
+#     "env"       = "test"
+#     "lifecycle" = "deleteme"
+#     "product"   = "runwhen"
+#   }
+# }
 
 # Outputs for Cluster FQDNs
 output "cluster_1_fqdn" {
@@ -152,15 +152,15 @@ output "cluster_1_rg" {
   value = azurerm_kubernetes_cluster.cluster_1_aks.resource_group_name
 }
 
-output "cluster_2_fqdn" {
-  value = azurerm_kubernetes_cluster.cluster_2_aks.fqdn
-}
-output "cluster_2_name" {
-  value = azurerm_kubernetes_cluster.cluster_2_aks.name
-}
-output "cluster_2_sub" {
-  value = var.subscription_id_2
-}
-output "cluster_2_rg" {
-  value = azurerm_kubernetes_cluster.cluster_2_aks.resource_group_name
-}
+# output "cluster_2_fqdn" {
+#   value = azurerm_kubernetes_cluster.cluster_2_aks.fqdn
+# }
+# output "cluster_2_name" {
+#   value = azurerm_kubernetes_cluster.cluster_2_aks.name
+# }
+# output "cluster_2_sub" {
+#   value = var.subscription_id_2
+# }
+# output "cluster_2_rg" {
+#   value = azurerm_kubernetes_cluster.cluster_2_aks.resource_group_name
+# }
